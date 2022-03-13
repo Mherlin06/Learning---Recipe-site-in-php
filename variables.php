49f8a1dc7562d067033c081c18c2f24b3386bbba
Hugues Mourice <mourice.hugues@gmail.com>
2022-03-13 19:11:28 +0100

    add comment section + recipes gestion

--- variables.php
+++ variables.php
@@ -14,3 +14,13 @@ if(isset($_GET['limit']) && is_numeric($_GET['limit'])) {
 } else {
     $limit = 100;
 }
+
+// Si le cookie est présent
+if (isset($_COOKIE['LOGGED_USER']) || isset($_SESSION['LOGGED_USER'])) {
+    $loggedUser = [
+        'email' => $_COOKIE['LOGGED_USER'] ?? $_SESSION['LOGGED_USER'],
+    ];
+}
+
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+$rootUrl = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
